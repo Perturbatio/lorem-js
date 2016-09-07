3d31aa7eb238d6cf137c28ebc6cea51c6be2b789
Kris Kelly <bertpotato@gmail.com>
2016-09-07 21:34:53 +0100

    removed duplicate typeof check removed stray comma from example

--- src/lorem.js
+++ src/lorem.js
@@ -49,7 +49,7 @@ export class Lorem {
 		this._rules = config['rules'] || [];
 		this.config = config;
 
-		if ( typeof config['data'] !== 'undefined' && typeof config['data'] !== 'undefined' ) {
+		if ( typeof config['data'] !== 'undefined') {
 			this._data = config['data'];
 		} else {
 			this._data = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr,  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,  sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
